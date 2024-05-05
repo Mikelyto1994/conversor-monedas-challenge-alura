82c8903a9e65912c822cfb948c02019335174f66
Mikelyto1994 <157014665+Mikelyto1994@users.noreply.github.com>
2024-05-05 01:14:20 -0500

    Update calcularMoneda.java
    Se adiciona 2 conceptos:
    1ero: Se le adiciona el parámetro para indicar al usuario de nuevo la opción que escogió antes de realizar el cálculo.
    2do: En caso de la excepción de que se coloque letras y no numeros, el programa diga ""Colocar número y no texto. Por favor, inténtelo de nuevo." y vuelva a solicitar el número para la conversión

--- src/com/aluracurso/conversormonedas/calcular/calcularMoneda.java
+++ src/com/aluracurso/conversormonedas/calcular/calcularMoneda.java
@@ -1,40 +1,45 @@
 package com.aluracurso.conversormonedas.calcular;
 
-
 import java.util.Scanner;
 
 public class calcularMoneda {
     private double conversionMoneda;
     private double cantidadAConvertir;
+    private Scanner entradaDato;
 
-    public double getConversionMoneda(){
+    public calcularMoneda() {
+        entradaDato = new Scanner(System.in);
+    }
+
+    public double getConversionMoneda() {
         return conversionMoneda;
-
-    }
-    Scanner entradaDato= new Scanner(System.in);
-    public void calcularDolaresAMonedas(double valorMoneda,String abreviatura){
-        cantidadAConvertir = ingresoCantidadAContervir();
-        conversionMoneda = valorMoneda*cantidadAConvertir;
-         System.out.println("El valor de " +cantidadAConvertir+ " [USD],  corresponde a el valor de " + conversionMoneda+ "["+abreviatura+"]");
-    }
-    public void calcularMonedasADolares(double valorMoneda, String abreviatura){
-       double cantidadAConvertir = ingresoCantidadAContervir();
-
-        conversionMoneda = cantidadAConvertir/valorMoneda;
-        System.out.println("El valor de " +cantidadAConvertir+ "["+abreviatura+"], corresponde a el valor de " + conversionMoneda +"[USD] \n");
     }
 
-    public double ingresoCantidadAContervir(){
-        try {
-            System.out.println("Ingrese el valor que desea convertir");
-            cantidadAConvertir = Double.valueOf(entradaDato.next());
+    public void calcularDolaresAMonedas(double valorMoneda, String abreviatura, String opcionSeleccionada) {
+        cantidadAConvertir = ingresoCantidadAConvertir(opcionSeleccionada);
+        conversionMoneda = valorMoneda * cantidadAConvertir;
+        System.out.println("El valor de " + cantidadAConvertir + " [USD], corresponde a el valor de " + conversionMoneda + "[" + abreviatura + "]");
+    }
 
-        }catch (NumberFormatException n){
-            System.out.println("Ocurrio un error: " + n.getMessage());
-        }catch (Exception e){
-            System.out.println("Ocurrio un error : "+e.getMessage());
+    public void calcularMonedasADolares(double valorMoneda, String abreviatura, String opcionSeleccionada) {
+        cantidadAConvertir = ingresoCantidadAConvertir(opcionSeleccionada);
+        conversionMoneda = cantidadAConvertir / valorMoneda;
+        System.out.println("El valor de " + cantidadAConvertir + "[" + abreviatura + "], corresponde a el valor de " + conversionMoneda + "[USD] \n");
+    }
+
+    public double ingresoCantidadAConvertir(String opcionSeleccionada) {
+        while (true) {
+            try {
+                System.out.println("Ingrese el valor que desea convertir: (Considerando que estás convirtiendo " + opcionSeleccionada + ")");
+                String input = entradaDato.next();
+                cantidadAConvertir = Double.parseDouble(input);
+                break; // Salir del bucle si se ingresó un número válido
+            } catch (NumberFormatException n) {
+                System.out.println("Colocar número y no texto. Por favor, inténtelo de nuevo.");
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error: " + e.getMessage());
+            }
         }
         return cantidadAConvertir;
     }
-
 }
